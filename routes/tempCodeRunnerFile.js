f2028a7790c48971a958b41b8f1720a6f07a25e6
Luis Diego Hernandez <pytests99@gmail.com>
2025-03-09 12:38:24 -0600

    Updated .gitignore, added backend/(week1) and .gitignore

--- routes/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-// GET all habits
-router.get('/allHabits', async (req, res) => {
-  try {
-    const habits = await Habit.find();
-    res.json(habits);
-  } catch (err) {
-    res.status(500).json({ message: 'Error retrieving habits', error: err.message });
-  }
-});
